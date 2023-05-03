1e6fd042613824a9a61a7afd852ad09619b9cf11
baderalouli <baderalouli04@gmail.com>
2023-05-04 00:26:15 +0100

    first commit

---
+++ sentiment/app1/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def HomePage(request):
+    pass
+
+
+
+def LoginPage(request):
+    return (request,'logup.html')
+
+
+
+
+
+def LogupPage(request):
+    return(request,'login.html')
